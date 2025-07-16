35344f1f8e5854de1897f091b548595e2c9b2e75
Vyachaslau Vasilets <vasilechek9786@gmail.com>
2025-07-16 18:36:51 +0300

    fix server

--- src/bot.js
+++ src/bot.js
@@ -1,26 +1,26 @@
-// require('dotenv').config();
-// const { Telegraf, Scenes, session } = require('telegraf');
-// const { tourWizard } = require('./scenes/tourWizard');
-
-// const bot = new Telegraf(process.env.BOT_TOKEN);
-
-// const stage = new Scenes.Stage([tourWizard]);
-// bot.use(session());
-// bot.use(stage.middleware());
-
-// bot.start((ctx) => ctx.scene.enter('tour-wizard'));
-// bot.hears('✏️ Заполнить новую заявку', (ctx) => ctx.scene.enter('tour-wizard'));
-
-// bot.launch();
-
-// process.once('SIGINT', () => bot.stop('SIGINT'));
-// process.once('SIGTERM', () => bot.stop('SIGTERM'));
 require('dotenv').config();
-const { Telegraf } = require('telegraf');
+const { Telegraf, Scenes, session } = require('telegraf');
+const { tourWizard } = require('./scenes/tourWizard');
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply('Бот запущен!'));
-bot.on('text', (ctx) => ctx.reply('Вы написали: ' + ctx.message.text));
+const stage = new Scenes.Stage([tourWizard]);
+bot.use(session());
+bot.use(stage.middleware());
+
+bot.start((ctx) => ctx.scene.enter('tour-wizard'));
+bot.hears('✏️ Заполнить новую заявку', (ctx) => ctx.scene.enter('tour-wizard'));
 
 bot.launch();
-console.log('Bot started');
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
+// require('dotenv').config();
+// const { Telegraf } = require('telegraf');
+// const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// bot.start((ctx) => ctx.reply('Бот запущен!'));
+// bot.on('text', (ctx) => ctx.reply('Вы написали: ' + ctx.message.text));
+
+// bot.launch();
+// console.log('Bot started');
